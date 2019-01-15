4af45526749bd9ba8cf04324d3ae39bad8be874c
Tsy97 <44691024+Tsy97@users.noreply.github.com>
2019-01-15 11:16:22 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,35 +3,48 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function( matrix ) {
-            m <<- matrix
-            i <<- NULL
-    }
-    get <- function() {
-    	m
-    }
-    setInverse <- function(inverse) {
-        i <<- inverse
-    }
-    getInverse <- function() {
-        i
-    }
-    list(set = set, get = get,
-         setInverse = setInverse,
-         getInverse = getInverse)
+makeMatrix<- function(x = matrix()){
+        mymatrix<- NULL
+        
+        ##set the value of the matrix
+        set<- function(y){
+                x<<- y
+                mymatrix<- NULL
+        }
+        
+        ##get the value of the matrix
+        get<- function() x
+        
+        ##set the value of the inversed matrix
+        setmatrix<- function(inversed.matrix) mymatrix<<- inversed.matrix
+        
+        ##get the value of the inversed matrix
+        getmatrix<- function() mymatrix
+        
+        ##return a list of all values
+        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
-
 cacheSolve <- function(x, ...) {
-  m <- x$getInverse()
- if( !is.null(m) ) {
-            message("getting cached data")
-            return(m)
-    }
-    data <- x$get()
-    m <- solve(data) %*% data
-    x$setInverse(m)
-    m
+        ##get the value returned by getmatrix function
+        mymatrix <- x$getmatrix()
+        
+        ##test if mymatrix has already been inversed, if true, just return the value
+        if(!is.null(mymatrix)) {
+                message("getting cached data")
+                return(mymatrix)
+        }
+        
+        ##if the value of mymatrix has not been inversed
+        ##get the value of the original matrix returned by get function
+        data <- x$get()
+        
+        ##cache the value of inversed matrix 
+        inversed.matrix <- solve(data, ...)
+        
+        ##set the value of inversed matrix to mymatrix variable
+        x$setmatrix(inversed.matrix)
+        
+        ##returen the value of inversed matrix
+        mymatrix
 }
